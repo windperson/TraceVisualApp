454b3f98575bade28eb90aecddba46a905eff939
Chen Yu Pao <windperson@gmail.com>
2019-05-01 10:05:40 +0800

    Add project files.

---
+++ TraceVisualApp/TraceVisualApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TraceVisualApp.Models;
+
+namespace TraceVisualApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
